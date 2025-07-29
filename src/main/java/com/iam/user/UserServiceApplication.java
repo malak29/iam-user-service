50f6781449efcd03e6fbb5ade0075c56634da6ca
Malak Parmar <malakparmar.29@gmail.com>
2025-07-28 23:24:59 -0400

    added enity scan from common util bean

--- src/main/java/com/iam/user/UserServiceApplication.java
+++ src/main/java/com/iam/user/UserServiceApplication.java
@@ -3,6 +3,7 @@ package com.iam.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.iam.user", "com.iam.common"})
 @EnableJpaRepositories(basePackages = "com.iam.user.repository")
+@EntityScan(basePackages = {"com.iam.common.model"})
 @EnableTransactionManagement
 public class UserServiceApplication {
     public static void main (String[] args) {
